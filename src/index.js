03968842a73cc34576f7c5a4972639c8329ad964
Antoniitemny <antoniitemny@yandex.ru>
2019-11-24 23:09:27 +0300

    hw3_corrected_2

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
-    if (Object.prototype.toString.call(array) !== '[object Array]' || array.length == 0) {
+    if (array instanceof Array !== true || array.length == 0) {
         throw new Error('empty array')
     } else if (typeof fn !== 'function') {
         throw new Error('fn is not a function')
@@ -53,7 +53,7 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
-    if (Object.prototype.toString.call(array) !== '[object Array]' || array.length == 0) {
+    if (array instanceof Array !== true || array.length == 0) {
         throw new Error('empty array')
     } else if (typeof fn !== 'function') {
         throw new Error('fn is not a function')
