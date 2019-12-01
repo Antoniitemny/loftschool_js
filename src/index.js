ceed4851eb3fa20d7605e7f098201b023b81d2b3
Antoniitemny <antoniitemny@yandex.ru>
2019-12-01 20:09:42 +0300

    hw5 corrected

--- src/index.js
+++ src/index.js
@@ -81,7 +81,11 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-    target.addEventListener('click', fn, { once: true });
+    target.addEventListener('click', remove);
+    function remove() {
+        fn();
+        target.removeEventListener('click', remove);
+    }
 }
 
 export {
